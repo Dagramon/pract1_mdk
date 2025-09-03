4f40110e3c0fcf03b8ab6ecd6915091b8795ff5b
tseneven <sashakuznetsov49@gmail.com>
2025-09-03 11:44:11 +0500

    1

---
+++ app/src/main/java/com/example/karamovb2_v3/LogIn.kt
@@ -0,0 +1,11 @@
+package com.example.karamovb2_v3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.log_in)
+    }
+}
